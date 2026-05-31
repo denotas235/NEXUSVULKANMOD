2e2c7f7afaa170af88ff0edfc418cf576047df5b
denotas235 <recebeumoneyantonio@gmail.com>
2026-05-31 12:12:00 +0200

    Refactor RenderSystemMixin for Vulkan integration

--- src/main/java/net/vulkanmod/mixin/render/RenderSystemMixin.java
+++ src/main/java/net/vulkanmod/mixin/render/RenderSystemMixin.java
@@ -1,63 +1,43 @@
 package net.vulkanmod.mixin.render;
 
-import com.mojang.blaze3d.shaders.ShaderType;
 import com.mojang.blaze3d.systems.GpuDevice;
 import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.*;
-import net.minecraft.client.renderer.DynamicUniforms;
-import net.minecraft.resources.Identifier;
+import net.minecraft.class_11282;
+import net.minecraft.class_12289;
 import net.vulkanmod.render.engine.VkGpuDevice;
 import net.vulkanmod.vulkan.Renderer;
 import net.vulkanmod.vulkan.VRenderSystem;
-import org.jetbrains.annotations.Nullable;
-import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
-import java.util.function.BiFunction;
-
-import static com.mojang.blaze3d.systems.RenderSystem.*;
-
 @Mixin(RenderSystem.class)
 public abstract class RenderSystemMixin {
-    @Shadow private static Matrix4f textureMatrix;
-    @Shadow private static @Nullable Thread renderThread;
 
-    @Shadow private static @Nullable GpuDevice DEVICE;
-    @Shadow private static @Nullable DynamicUniforms dynamicUniforms;
-
+    @Shadow private static Thread renderThread;
+    @Shadow private static GpuDevice DEVICE;
     @Shadow private static String apiDescription;
+    @Shadow private static class_11282 dynamicUniforms;
 
     @Shadow
     public static void assertOnRenderThread() {}
 
+    /**
+     * @author VulkanMod fork
+     * @reason Replace OpenGL renderer with Vulkan
+     */
     @Overwrite(remap = false)
-    public static void initRenderer(long l, int i, boolean bl, BiFunction<Identifier, ShaderType, String> shaderSource, boolean bl2) {
+    public static void initRenderer(long window, int debugVerbosity, boolean bl,
+            class_12289 shaderSource, boolean bl2) {
         renderThread.setPriority(Thread.NORM_PRIORITY + 2);
 
         VRenderSystem.initRenderer();
 
-        DEVICE = new VkGpuDevice(l, i, bl, shaderSource, bl2);
-        apiDescription = getDevice().getImplementationInformation();
+        DEVICE = new VkGpuDevice(window, debugVerbosity, bl, shaderSource, bl2);
+        apiDescription = RenderSystem.getDevice().getImplementationInformation();
 
         Renderer.initRenderer();
 
-        dynamicUniforms = new DynamicUniforms();
+        dynamicUniforms = new class_11282();
     }
-
-    @Overwrite(remap = false)
-    public static void setTextureMatrix(Matrix4f matrix4f) {
-        assertOnRenderThread();
-        textureMatrix.set(matrix4f);
-        VRenderSystem.setTextureMatrix(matrix4f);
-    }
-
-    @Overwrite(remap = false)
-    public static void resetTextureMatrix() {
-        assertOnRenderThread();
-        textureMatrix.identity();
-        VRenderSystem.setTextureMatrix(textureMatrix);
-    }
-
 }
